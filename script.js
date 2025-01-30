3d19527fade053d6036dba5a48c0e0c1a4042527
Mateus Oliveira <warface01031999@gmail.com>
2025-01-30 16:53:34 -0300

    initial javascript

---
+++ script.js
@@ -0,0 +1,8 @@
+const  serachInput = document.getElementById('search-input');
+
+document.addEventListener('input', function() {
+     const  searchTerm = serachInput.value.toLoweCase();
+     if (searchTerm === '') {
+        
+     }
+})
